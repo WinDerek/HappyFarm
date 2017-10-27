1674c3efc27320ab7f916345a0bb25957c6b6a7d
DerekDick <frankderekdick@gmail.com>
2017-10-28 01:00:04 +0800

    Trivial change.

--- src/main/java/com/haveacupofjava/happyfarm/farmhand/AbstractFarmHand.java
+++ src/main/java/com/haveacupofjava/happyfarm/farmhand/AbstractFarmHand.java
@@ -12,8 +12,9 @@ public abstract class AbstractFarmHand {
      * @param tool
      * @return
      */
-    public AbstractTool getTool(String tool){
+    protected AbstractTool getTool(String tool){
         StorageRoom storageRoom = StorageRoom.getInstant();
         return storageRoom.getTool(tool);
     }
+
 }
