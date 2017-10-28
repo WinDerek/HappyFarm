a2012feb1446bcd1842b9be2012f2701b7491e98
DerekDick <frankderekdick@gmail.com>
2017-10-28 23:41:24 +0800

    Duplicate files removed.

--- src/main/java/com/haveacupofjava/happyfarm/trade/FactoryMediator.java
+++ src/main/java/com/haveacupofjava/happyfarm/trade/FactoryMediator.java
@@ -1,8 +1,32 @@
 package com.haveacupofjava.happyfarm.trade;
 
+import com.haveacupofjava.happyfarm.factory.AbstractFactory;
+
 public class FactoryMediator extends AbstractMediator {
+
+    private Class clazz;
+    private Integer number;
+
     @Override
     public void handleTrade() {
+        AbstractFactory abstractFactory = null;
 
+        try {
+            abstractFactory = (AbstractFactory) Class.forName(clazz.getName()).newInstance();
+        } catch (Exception exception) {
+            exception.printStackTrace(System.out);
+        }
     }
+
+    /**
+     * Sets the trade information
+     * @param clazz The Class of AbstractFactory
+     * @param number The number of AbstractCreature to buy
+     */
+    public void setTradeInfo(Class clazz, int number) {
+        this.clazz = clazz;
+        this.number = number;
+    }
+
 }
+
