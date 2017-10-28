3221e61e7d1fdd6c5d1b2ef8497c97022fb437a0
wzes <793890838@qq.com>
2017-10-28 21:53:03 +0800

    merge code derek

--- src/test/java/TestApplication1.java
+++ src/test/java/TestApplication1.java
@@ -8,7 +8,6 @@ import com.haveacupofjava.happyfarm.task.Request;
 public class TestApplication1 {
 
     public static void main(String[] args) {
-<<<<<<< HEAD
         Farmer farmer = Farmer.getInstance();
         HappyFarm happyFarm = HappyFarm.getInstance();
         happyFarm.showFieldsInformation();
@@ -22,21 +21,6 @@ public class TestApplication1 {
             exception.printStackTrace(System.out);
         }
         System.out.println("The last statement executed.");
-=======
-//        Farmer farmer = Farmer.getInstance();
-//        HappyFarm happyFarm = HappyFarm.getInstance();
-//        happyFarm.showFieldsInformation();
-//        PigPen pigPen = new PigPen();
-//        Sheep sheep = new Sheep();
-//        pigPen.addAnimal(sheep);
-//        pigPen.feedAnimals();
-//        try {
-//            sheep.feed();
-//        } catch (MethodExposedException exception) {
-//            exception.printStackTrace(System.out);
-//        }
-//        System.out.println("The last statement executed.");
->>>>>>> f2b0bf190c91d7e096d819748ea0dd50cd1bdf7a
     }
 
 }
